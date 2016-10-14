0c32ccf797cd3c18cd6f7ee90e5022f7bd014512
wingjay <wingjaysjtu@gmail.com>
2016-10-14 17:29:58 +0800

    indent fix

--- app.py
+++ app.py
@@ -18,23 +18,24 @@ def index():
 
 @app.route('/webhook', methods=['GET', 'POST'])
 def webhook():
-	req = request.get_json(silent=True, force=True)
+    req = request.get_json(silent=True, force=True)
 
-	print("Request:")
-	print(json.dumps(req, indent=4))
+    print("Request:")
+    print(json.dumps(req, indent=4))
 
-	ai_action = req.get("result").get("action")
-	if ai_action:
-		result = process(ai_action)
-	else:
-		result = {'test':1, 'test2':2}
+    ai_action = req.get("result").get("action")
+    result = ''
+    if ai_action:
+        result = process(ai_action)
+    else:
+        result = {'speech': None, 'displayText': None}
 
-	response = make_response(result)
-	response.headers['Content-Type'] = 'application/json'
-
+    response = make_response(result)
+    response.headers['Content-Type'] = 'application/json'
+    
     print("Response:")
     print(json.dumps(response, indent=4))
-	return response
+    return response
 
 
 # Every service must provide a function: process(action) and return 
@@ -46,44 +47,45 @@ def webhook():
 #     "source": "wingjay-github-apiai-weather-webhook-sample"
 # }
 def process(action):
-	print "process action"
-	print action
-    if action=='get_weather_for_location':
+    print "process action"
+    print action
+    result = ''
+    if action == 'get_weather_for_location':
         result = service.weather.process(action)
-	return result
+    return result
 
 
 def _mock_request_from_apiai():
-	return {
-    "originalRequest": None, 
-    "result": {
-        "fulfillment": {
-            "speech": ""
-        }, 
-        "speech": "", 
-        "source": "agent", 
-        "actionIncomplete": False, 
-        "resolvedQuery": "weather in London", 
-        "action": "yahooWeatherForecast", 
-        "score": 1.0, 
-        "metadata": {
-            "webhookUsed": "true", 
-            "intentId": "471fd4d3-d755-40ac-bcb7-85f01c404f57", 
-            "intentName": "weather"
-        }, 
-        "parameters": {
-            "geo-city": "London"
-        }, 
-         "contexts": []
-     	}, 
-	    "timestamp": "2016-10-13T07:49:21.419Z", 
-	    "sessionId": "81e229ed-d013-4b19-9ef6-7b1cdd5827d9", 
-	    "id": "f9c03e3b-97b3-4d38-b2ca-d83b0598d5d2", 
-	    "status": {
-	        "errorType": "success", 
-	        "code": 200
-	    }
-	}
+    return {
+        "originalRequest": None,
+        "result": {
+            "fulfillment": {
+                "speech": ""
+            },
+            "speech": "",
+            "source": "agent",
+            "actionIncomplete": False,
+            "resolvedQuery": "weather in London",
+            "action": "yahooWeatherForecast",
+            "score": 1.0,
+            "metadata": {
+                "webhookUsed": "true",
+                "intentId": "471fd4d3-d755-40ac-bcb7-85f01c404f57",
+                "intentName": "weather"
+            },
+            "parameters": {
+                "geo-city": "London"
+            },
+            "contexts": []
+        },
+        "timestamp": "2016-10-13T07:49:21.419Z",
+        "sessionId": "81e229ed-d013-4b19-9ef6-7b1cdd5827d9",
+        "id": "f9c03e3b-97b3-4d38-b2ca-d83b0598d5d2",
+        "status": {
+            "errorType": "success",
+            "code": 200
+        }
+    }
 
 if __name__ == '__main__':
     port = int(os.getenv('PORT', 5000))
